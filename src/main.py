f7780e49cc5975cd5b9b9df813b57c8205d773f2
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-07 17:57:10 +0700

    fix: add A star test to main

--- src/main.py
+++ src/main.py
@@ -2,13 +2,18 @@ from algorithm.AStar import AStar
 from algorithm.util.Utility import parse_file
 
 def main():
-    total_nodes, nodes, adjacency_matrix = parse_file('test_parser.txt')
+    total_nodes, nodes, adjacency_matrix = parse_file('test/tc1.txt')
     print(f'Total nodes: {total_nodes}')
     print('Nodes:')
     for name, coord in nodes:
         print(f'  {name}: {coord}')
     print('Adjacency matrix:')
     print(adjacency_matrix)
+
+    print("Solve for A Star")
+    a_star = AStar(adjacency_matrix, total_nodes, nodes)
+    a_star.solve("point_A", "point_D")
+    a_star.get_result_route()
 
 if __name__ == '__main__':
     main()
