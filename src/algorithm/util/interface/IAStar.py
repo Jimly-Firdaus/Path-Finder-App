a49fa4fd5df0ba4fe70ef9d9633150f5a9864307
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-07 11:17:55 +0700

    style: fix solve doxygen

--- src/algorithm/util/interface/IAStar.py
+++ src/algorithm/util/interface/IAStar.py
@@ -2,8 +2,8 @@ from abc import ABC, abstractmethod
 
 class IAStar:
     #  Solve for A* algorithm.
-    #  @param start_node starting node (integer).
-    #  @param goal_node goal node (integer).
+    #  @param start_node starting node name (string).
+    #  @param goal_node goal node name (string).
     #  @returns path to goal, total steps taken
     @abstractmethod
     def solve(self, start_node, goal_node):
