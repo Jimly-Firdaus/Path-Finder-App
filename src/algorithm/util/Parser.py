02140726eeb140d699ee0b8630bc9328a6d7b5e3
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-07 10:27:14 +0700

    feat: add parser text file

---
+++ src/algorithm/util/Parser.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def parse_file(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        total_nodes = int(lines[0])
+        nodes = []
+        for i in range(1, total_nodes * 2 + 1, 2):
+            name = lines[i].strip()
+            coord = tuple(map(float, lines[i + 1].strip().split()))
+            nodes.append((name, coord))
+        adjacency_matrix = np.array([list(map(int, line.strip().split())) for line in lines[total_nodes * 2 + 1:]])
+    return total_nodes, nodes, adjacency_matrix
