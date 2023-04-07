6a8b5eece9fac6d137db8012083de6c28658fe3b
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-07 11:16:23 +0700

    feat: add haversine to measure distance

--- src/algorithm/util/Utility.py
+++ src/algorithm/util/Utility.py
@@ -12,3 +12,19 @@ def parse_file(filename):
             nodes.append((name, coord))
         adjacency_matrix = np.array([list(map(int, line.strip().split())) for line in lines[total_nodes * 2 + 1:]])
     return total_nodes, nodes, adjacency_matrix
+
+def d_haversine (latitude_1, longitude_1, latitude_2, longitude_2):
+    earth_rad = 6371
+
+    # conversion
+    lat1, lon1, lat2, lon2 = map(radians, [latitude_1, longitude_1, latitude_2, longitude_2])
+
+    # find d
+    d_latitude = lat2 - lat1
+    d_longitude = lon2 - lon1
+
+    # harversine distance
+    haversine = sin(d_latitude / 2) ** 2 + cos(lat1) * cos(lat2) * sin(d_longitude / 2) ** 2
+    central_angle = 2 * atan2(sqrt(haversine), sqrt(1 - haversine))
+    haversine_distance = earth_rad * central_angle
+    return haversine_distance
