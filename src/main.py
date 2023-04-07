b254b47219f170e78ae3300ede66b7128f4b9a01
RyanSC06 <cadanganryansc@gmail.com>
2023-04-07 22:39:36 +0700

    Add UCS function and testing

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
-from algorithm.AStar import AStar
+# from algorithm.AStar import AStar
 from algorithm.util.Utility import parse_file
 
 def main():
-    total_nodes, nodes, adjacency_matrix = parse_file('test_parser.txt')
+    total_nodes, nodes, adjacency_matrix = parse_file('test/test_parser.txt')
     print(f'Total nodes: {total_nodes}')
     print('Nodes:')
     for name, coord in nodes:
+        # print (type(name))
+        # print (type(coord))
         print(f'  {name}: {coord}')
     print('Adjacency matrix:')
     print(adjacency_matrix)
 
+    x = "AKU "
+    y = "ADALAH"
+    print (x+y)
+    
 if __name__ == '__main__':
     main()
