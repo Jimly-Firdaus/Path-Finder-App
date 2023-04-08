83bd152d21cb102a171e415489797a9010af9800
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-08 18:06:59 +0700

    feat: add dropdown

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,11 +9,34 @@
         effort by always taking the most efficient route.
       </h1>
     </BaseCard>
-    <q-file color="teal" filled v-model="file" label="Label">
-      <template v-slot:prepend>
-        <q-icon name="cloud_upload" />
-      </template>
-    </q-file>
+    <div class="tw-ml-4 tw-gap-4 tw-w-72">
+      <q-file
+        color="teal"
+        filled
+        v-model="file"
+        label="Label"
+        @update:modelValue="handleFileUpload"
+        class="tw-w-full"
+      >
+        <template v-slot:prepend>
+          <q-icon name="cloud_upload" />
+        </template>
+      </q-file>
+      <q-select
+        v-model="source"
+        :options="mapData"
+        label="Choose a start point"
+        @update:model-value="updateDestOption"
+        class="tw-w-full"
+      />
+      <q-select
+        v-model="dest"
+        :options="destOption"
+        label="Choose a destination"
+        :disable="source === null"
+        class="tw-w-full"
+      />
+    </div>
     <BaseBtn label="Find!" @click="getFile"> </BaseBtn>
     <!-- Test Map here -->
     <GoogleMaps />
@@ -21,24 +44,55 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import axios from 'axios';
 
-const file = ref(null);
+const file = ref<File | null>(null);
+const source = ref<string | null>(null);
+const dest = ref<string | null>(null);
+const mapData = ref<string[]>([]);
+const destOption = computed(() =>
+  mapData.value.filter((item) => item !== source.value)
+);
+
+const updateDestOption = () => {
+  dest.value = null;
+};
+
+const handleFileUpload = (file: File) => {
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const text = event.target?.result as string;
+      const lines = text.split('\n');
+      mapData.value = lines.filter(
+        (_, index) =>
+          index > 0 && index % 2 !== 0 && index < parseInt(lines[0]) * 2
+      );
+    };
+    reader.readAsText(file);
+  }
+};
 
 const getFile = () => {
   if (file.value) {
     const reader = new FileReader();
     reader.onload = async (event) => {
-      const text = event.target?.result;
+      const text = event.target?.result as string;
+      console.log(source.value);
+      console.log(dest.value);
       const data = {
         text,
-        src: "bolu_meranti",
-        dest: "methodist_3",
-      }
-      const response = await axios.post('http://localhost:5000/solve', JSON.stringify(data), {
-        headers: { 'Content-Type': 'application/json' },
-      });
+        src: source.value,
+        dest: dest.value,
+      };
+      const response = await axios.post(
+        'http://localhost:5000/solve',
+        JSON.stringify(data),
+        {
+          headers: { 'Content-Type': 'application/json' },
+        }
+      );
       const result = response.data;
       console.log(result);
     };
