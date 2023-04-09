f4b81c9531a795adccba21006700f261f998ba6d
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-09 12:29:11 +0700

    fix: attempt to merge UCS

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from src.algorithm.util.Utility import parse_file
 from src.algorithm.AStar import AStar
+from src.algorithm.UCS import UCS
 
-def main(text: str, src: str, dest: str):
+def main(text: str, src: str, dest: str, solver: str):
     total_nodes, nodes, adjacency_matrix = parse_file(text)
     print(f'Total nodes: {total_nodes}')
     print('Nodes:')
@@ -10,8 +11,12 @@ def main(text: str, src: str, dest: str):
     print('Adjacency matrix:')
     print(adjacency_matrix)
 
-    print("Solve for A Star")
-    a_star = AStar(adjacency_matrix, total_nodes, nodes)
-    a_star.solve(src, dest)
-    return a_star.get_result_route()
+    if (solver == "UCS"):
+        path, cost = UCS(adjacency_matrix, total_nodes, nodes, src, dest)
+        return (cost, path)
+    else:
+        print("Solve for A Star")
+        a_star = AStar(adjacency_matrix, total_nodes, nodes)
+        a_star.solve(src, dest)
+        return a_star.get_result_route()
 
