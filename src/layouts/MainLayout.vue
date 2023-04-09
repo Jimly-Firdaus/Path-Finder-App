19812f2a131745f24c4567f2e62385f42f14c03a
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-09 16:36:25 +0700

    feat: add copyright

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,25 +11,15 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-          Path Finder App
-        </q-toolbar-title>
+        <q-toolbar-title> Path Finder App </q-toolbar-title>
 
         <div>With Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list class="space-between">
+        <q-item-label header> Essential Links </q-item-label>
 
         <EssentialLink
           v-for="link in essentialLinks"
@@ -37,6 +27,11 @@
           v-bind="link"
         />
       </q-list>
+      <q-item>
+        <q-item-section>
+          © 2023 by Jimly Firdaus <q-space /> © 2023 by Ryan Samuel Chandra
+        </q-item-section>
+      </q-item>
     </q-drawer>
 
     <q-page-container>
@@ -46,21 +41,25 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
+import { onMounted, ref } from 'vue';
+import EssentialLink, {
+  EssentialLinkProps,
+} from 'components/EssentialLink.vue';
 
 const essentialLinks: EssentialLinkProps[] = [
   {
     title: 'Source Code',
     caption: 'Github',
     icon: 'code',
-    link: 'https://github.com/Jimly-Firdaus/Tucil3_13521102_13521140'
+    link: 'https://github.com/Jimly-Firdaus/Tucil3_13521102_13521140',
   },
 ];
 
-const leftDrawerOpen = ref(false)
+const leftDrawerOpen = ref(false);
+
+onMounted(() => (leftDrawerOpen.value = false));
 
 function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+  leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 </script>
