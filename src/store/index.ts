9deed8d98c5c66c224781181cc0a2019eed89ea1
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-10 22:15:51 +0700

    feat: add all position to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,20 +3,28 @@ import { Path } from 'src/composables';
 
 interface State {
   pathRetrieved: Path[];
+  allPosition: Path[];
 }
 
 export default createStore<State>({
   state: {
     pathRetrieved: [],
+    allPosition: [],
   },
   mutations: {
     setPathRetrieved(state, pathRetrieved: Path[]) {
       state.pathRetrieved = pathRetrieved;
     },
+    setAllPosition(state, allPosition: Path[]) {
+      state.allPosition = allPosition;
+    },
   },
   actions: {
     updatePathRetrieved({ commit }, pathRetrieved: Path[]) {
       commit('setPathRetrieved', pathRetrieved);
+    },
+    updateAllPosition({ commit }, allPosition: Path[]) {
+      commit('setAllPosition', allPosition);
     },
   },
 });
